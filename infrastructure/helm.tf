0458675a7a4287c5fc1746f228b61d154192cb68
Dipanshu <dipanshubisen15@gmail.com>
2026-06-30 11:55:47 +0530

    add helm

---
+++ infrastructure/helm.tf
@@ -0,0 +1,21 @@
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.my-cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.my-cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.my-cluster.token
+  }
+}
+
+data "aws_eks_cluster_auth" "my-cluster" {
+  name = aws_eks_cluster.my-cluster.name
+}
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+
+  depends_on = [ aws_eks_node_group.my-workers]
+}
